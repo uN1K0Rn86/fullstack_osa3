ef097261debd37e8a94a06c4ff7de1bd653f9a82
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-01-09 10:02:50 +0200

    Part 3 exercise 5

--- index.js
+++ index.js
@@ -62,6 +62,22 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end
 })
 
+const generateId = () => String(Math.floor(Math.random() * 1000000))
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
